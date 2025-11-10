91e53a54ec14bb812822e6481ef6b8d5d02deaca
teuzada <163007822+teuzada@users.noreply.github.com>
2025-11-10 11:53:42 -0300

    vxxvcx

--- script.js
+++ script.js
@@ -24,3 +24,8 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
+window.addEventListener("scroll", () => {
+    const header = document.querySelector("header");
+    header.classList.toggle("scrolled", window.scrollY > 50);
+});
+
